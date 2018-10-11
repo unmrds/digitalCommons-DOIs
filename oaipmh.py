064bd0fa6cf36619ccab76e03a7590c00fd51cd2
Jon Wheeler <jwheel01@unm.edu>
2018-10-11 16:12:46 -0600

    removed redundant resumption function

--- oaipmh.py
+++ oaipmh.py
@@ -2,10 +2,13 @@ import requests
 from xml.etree import ElementTree
 
 
-def oairesume(oaiurl, token):
+def oaiharvest(oaiurl, getset='', token=''):
     ns = {'dc_oai': 'http://www.openarchives.org/OAI/2.0/',
           'dc': 'http://purl.org/dc/elements/1.1/'}
-    payload = {'verb': 'ListRecords', 'resumptionToken' : token}
+    if getset != '':
+        payload = {'verb': 'ListRecords', 'metadataPrefix': 'dcs', 'set': getset}
+    elif token != '':
+        payload = {'verb': 'ListRecords', 'resumptionToken': token}
     r = requests.get(oaiurl, params=payload)
     #print(r.url)
     tree = ElementTree.fromstring(r.content)
@@ -17,29 +20,11 @@ def oairesume(oaiurl, token):
         print('no more requests')
     else:
         print(token.text)
-        oairesume(oaiurl, token.text)
-
-
-def oaiharvest(oaiurl, getset):
-    ns = {'dc_oai': 'http://www.openarchives.org/OAI/2.0/',
-          'dc': 'http://purl.org/dc/elements/1.1/'}
-    payload = {'verb': 'ListRecords', 'metadataPrefix': 'dcs', 'set': getset}
-    r = requests.get(oaiurl, params=payload)
-    #print(r.url)
-    tree = ElementTree.fromstring(r.content)
-    recs = tree.find('dc_oai:ListRecords', ns)
-    items = recs.findall('dc_oai:record', ns)
-    print(len(items))
-    token = recs.find('dc_oai:resumptionToken', ns)
-    if token.text is None:
-        print('no more requests')
-    else:
-        print(token.text)
-        oairesume(oaiurl, token.text)
+        oaiharvest(oaiurl, token=token.text)
 
 
 oaiurl = "http://digitalrepository.unm.edu/do/oai/"
 
 getset = "publication:nawrs"
 
-oaiharvest(oaiurl, getset)
+oaiharvest(oaiurl, getset=getset)
