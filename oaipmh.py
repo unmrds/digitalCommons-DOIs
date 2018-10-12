4c6785a197a53e10f3b5a378f7e342363fdef85b
Jon Wheeler <jwheel01@unm.edu>
2018-10-12 11:42:43 -0600

    writing response xml to file

--- oaipmh.py
+++ oaipmh.py
@@ -2,7 +2,13 @@ import requests
 from xml.etree import ElementTree
 
 
-def oaiharvest(oaiurl, getset='', token=''):
+def writexml(xmldata, fc):
+    with open('output/result_'+str(fc)+'.xml', 'w') as o:
+        o.write(xmldata)
+        o.close
+    return
+
+def oaiharvest(oaiurl, getset='', token='', fc=1):
     ns = {'dc_oai': 'http://www.openarchives.org/OAI/2.0/',
           'dc': 'http://purl.org/dc/elements/1.1/'}
     if getset != '':
@@ -10,19 +16,23 @@ def oaiharvest(oaiurl, getset='', token=''):
     elif token != '':
         payload = {'verb': 'ListRecords', 'resumptionToken': token}
     r = requests.get(oaiurl, params=payload)
+    r.encoding = 'utf-8'
     root = ElementTree.fromstring(r.content)
     recs = root.find('dc_oai:ListRecords', ns)
     items = recs.findall('dc_oai:record', ns)
-    print(len(items))
-    token = recs.find('dc_oai:resumptionToken', ns)
+    print(str(len(items)) + " items in current set")
+    token = recs.find("dc_oai:resumptionToken", ns)
     if token.text is None:
+        writexml(r.text, fc)
         print('no more requests')
     else:
-        oaiharvest(oaiurl, token=token.text)
+        writexml(r.text, fc)
+        fc += 1
+        oaiharvest(oaiurl, token=token.text, fc=fc)
 
 
 oaiurl = "http://digitalrepository.unm.edu/do/oai/"
 
-getset = "publication:nawrs"
+getset = "publication:energizenm"
 
 oaiharvest(oaiurl, getset=getset)
